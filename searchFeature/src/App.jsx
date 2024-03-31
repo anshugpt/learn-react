2ea766ccba2302c501a04de19a85630871e81059
anshugpt <anshugupta@gmail.com>
2024-03-31 11:34:41 +0530

    added code

---
+++ searchFeature/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="flex w-full items-center space-x-2 md:w-1/3">
+      <input
+        className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+        type="search"
+        placeholder="search"
+      ></input>
+      <button
+        type="button"
+        className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+      >
+        Search
+      </button>
+    </div>
+    </>
+  )
+}
+
+export default App
