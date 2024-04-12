039ba6936c3866bd782658100797926a6734db57
anshugpt <anshugupta@gmail.com>
2024-04-12 11:27:36 +0530

    added basic context code

--- contextApiWithLocalStorage/src/context/TodoContext.js
+++ contextApiWithLocalStorage/src/context/TodoContext.js
@@ -1,6 +1,18 @@
 import React, { createContext, useContext } from "react";
 
-export const TodoContext = createContext({})
+export const TodoContext = createContext({
+    todos : [
+        {
+            id : 1,
+            todo : "Todo msg",
+            completed : false
+        }
+    ],
+    addTodo : (todo) => {},
+    updateTodo : (id, todo) => {},
+    deleteTodo : (id) => {},
+    toggleTodo : (id) => {}
+})
 
 export const TodoContextProvider = TodoContext.Provider
 
