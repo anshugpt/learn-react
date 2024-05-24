d2e9c4c4247ab1c1d937f6f59a70609ef61834f1
anshugpt <anshugupta@gmail.com>
2024-05-24 17:07:13 +0530

    mixed project is succesfull

---
+++ mixedProjectPractice/src/context/TodoContext.jsx
@@ -0,0 +1,17 @@
+import React, { createContext, useContext } from "react";
+
+export const TodoContext = createContext([
+    {
+        todos: [{}],
+        addTodo: (todo) => {},
+        updateTodo: (id, todo) => {},
+        deleteTodo: (id) => {},
+        toggleComplete: (id) => {}
+    }
+])
+
+export const TodoProvider = TodoContext.Provider
+
+export default function useTodo(){
+    return useContext(TodoContext)
+}
---
+++ mixedProjectPractice/src/main.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import  { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './layout/Layout.jsx'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "about",
+        element: <About/>
+      },
+      {
+        path: "contact",
+        element: <Contact/>
+      },
+      {
+        path: "todo",
+        element : <App/>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
